3635682c0f6ee764158ac9470b2f95060f0e02e3
kellycoles <kellycoles1@gmail.com>
2019-10-14 13:39:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace customColorsAndSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
